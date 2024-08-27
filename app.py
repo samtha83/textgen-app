6b3079b561ed39d8f0ea8ac2452674a94ef344ed
Samtha Reddy <samtha.learn@gmail.com>
2024-08-26 20:40:27 -0400

    add generate code

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI 
 from pydantic import BaseModel
 import uvicorn
-
+from transformers import pipeline
 
 
 app = FastAPI()
@@ -17,8 +17,23 @@ def welcome():
 
 @app.get("/home") 
 def welcome():
-    return {"message": "welcome home"}
+    return {"message": "Welcome to  AI Application"}
+
+@app.post("/textclassify")
+def classify(prompt: Body):
+    classifier = pipeline('text-classification')
+    output = classifier(prompt.text)
+    return {"class": output[0]['label']}
+
+
+@app.post("/generate")
+def generation(prompt: Body):
+    generator =  pipeline('text-generation', model = 'gpt2', max_length=200)
+    output = generator(prompt.text)
+    return output[0]['generated_text']
+
+
 
 if __name__ == "__main__":
-    print("HELLO")
+    
     uvicorn.run(app,host="0.0.0.0",port=80)
